8736a349f4a8f81d3a0c73906499a4c608b8e94d
Marty <simplon@simplon-Latitude-E6410>
2017-04-19 16:52:39 +0200

    WiP

--- github_prairie/src/Main.java
+++ github_prairie/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		system.out.println("je que veux");
+		System.out.println("je que veux");
 	}
 
 }
